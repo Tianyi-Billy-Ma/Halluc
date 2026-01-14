b74eb7ca608c0b04693dbab60069ab42e6f81f53
Tianyi-Billy-Ma <159465837+HypergraphResearcher@users.noreply.github.com>
2026-01-14 18:19:27 -0500

    Revise the ideal task to squad_v2 for vanilla

--- scripts/crc/ideal.sh
+++ scripts/crc/ideal.sh
@@ -5,5 +5,7 @@ conda activate llmhalluc
 
 # accelerate launch -m lm_eval --model hf --model_args pretrained=meta-llama/Llama-3.2-3B --tasks gsm8k_cot_simple --output_path outputs/llama-3.2-3b/gsm8k/vanilla/eval/gsm8k_cot_simple/results.json --include_path ./configs/lm_eval/tasks --log_samples --wandb_args name=llama-3.2-3b_gsm8k_cot_simple_vanilla,project=Halluc
 
-accelerate launch -m llmhalluc.run_exp --config configs/llmhalluc/sft.yaml
+# accelerate launch -m llmhalluc.run_exp --config configs/llmhalluc/sft.yaml
+
+accelerate launch -m lm_eval --model hf --model_args pretrained=meta-llama/Llama-3.2-1B --tasks squadv2 --output_path ./outputs/llama-3.2-1b/squadv2/vanilla/eval/squadv2/results.json --include_path ./configs/lm_eval/tasks --log_samples --wandb_args name=llama-3.2-1b_squadv2_vanilla,project=Halluc
 

3ee0dd349ad05c986b49b6e5812c1f9203651e73
Tianyi-Billy-Ma <159465837+HypergraphResearcher@users.noreply.github.com>
2026-01-15 15:54:29 -0500

    fix chat completion script

--- scripts/crc/ideal.sh
+++ scripts/crc/ideal.sh
@@ -5,7 +5,7 @@ conda activate llmhalluc
 
 python -m llmhalluc.scripts.chat_completion \
     --model_name_or_path meta-llama/Llama-3.2-1b \
-    --tokenizer_name_or_path meta-llama/Llama-3.2-1b \
+    --tokenizer_name_or_path /users/tma2/Projects/Halluc/outputs/llama-3.2-1b/gsm8k_masked_backtrack/sft/lora/train \
     --adapter_name_or_path /users/tma2/Projects/Halluc/outputs/llama-3.2-1b/gsm8k_masked_backtrack/sft/lora/train \
     --message "What does <|reserved_special_token_0|> means?"
 

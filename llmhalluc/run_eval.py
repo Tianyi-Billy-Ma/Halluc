0a26064799650d3e474069cb918bed8134f7337d
Tianyi-Billy-Ma <159465837+HypergraphResearcher@users.noreply.github.com>
2026-01-16 14:56:09 -0500

    fix run_eval function error

--- llmhalluc/run_eval.py
+++ llmhalluc/run_eval.py
@@ -4,6 +4,8 @@ Usage:
     python -m llmhalluc.run_eval
 """
 
+import sys
+import os
 from pathlib import Path
 
 from llmhalluc.eval import run_eval
@@ -16,11 +18,12 @@ DEFAULT_CONFIG_PATH = REPO_ROOT / "configs" / "llmhalluc" / "e2e.yaml"
 
 def main():
     setup_logging(verbose=False)
-    
-    config_path, cli_args = parse_config_arg(sys.args[1:],DEFAULT_CONFIG_PATH)
+
+    config_path, cli_args = parse_config_arg(sys.argv[1:], DEFAULT_CONFIG_PATH)
 
     setup_dict = e2e_cfg_setup(DEFAULT_CONFIG_PATH, save_cfg=True)
 
+    hf_args = setup_dict.args.hf_args
     wandb_project = getattr(hf_args, "wandb_project", None)
     if wandb_project:
         os.environ["WANDB_PROJECT"] = wandb_project

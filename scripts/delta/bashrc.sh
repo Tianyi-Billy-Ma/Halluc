c2f34d0c674b485dd1b8ce847fe4c8f257e238bc
Billy Ma <tma2@nd.edu>
2026-01-20 21:09:49 -0500

    fix delta wandb error

--- scripts/delta/bashrc.sh
+++ scripts/delta/bashrc.sh
@@ -1,3 +1,13 @@
 module load pytorch-conda/2.8
-
+module load aws-ofi-nccl/1.14.2
 conda activate llmhalluc
+
+# Weights & Biases Configuration for Delta
+# export WANDB_MODE=offline
+# export WANDB_SERVICE_WAIT=300
+export WANDB_DIR=/work/hdd/bgdn/tma3/.cache/wandb
+mkdir -p $WANDB_DIR
+
+# HuggingFace Cache
+export HF_HOME=/work/hdd/bgdn/tma3/.cache/huggingface
+mkdir -p $HF_HOME
